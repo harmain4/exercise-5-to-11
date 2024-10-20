3d040bcaad4a8145312e1bc9366ec8ef75fd1c85
harmain4 <affanmohammad669@gmail.com>
2024-10-20 23:08:21 +0500

    6 to11

---
+++ app.js
@@ -0,0 +1,89 @@
+//   chapter 6 
+//   question 1
+
+// let a = 10;
+// document.write(` <h1> result </h1> <br> The value of a is: ${a}<br> <br>`);
+
+
+// document.write(` The value of ++a is: ${++a}<br>`);
+// document.write(` Now the value of a is: ${a}<br> <br>`)
+
+// document.write(` The value of a++ is: ${a++}<br>`);
+// document.write(` Now the value of a is: ${a}<br> <br>`);
+
+
+// document.write(` Now the value of --a is: ${--a}<br>`);
+// document.write(` Now the value of a is: ${a}<br> <br>`);
+
+
+// document.write(` Now the value of a-- is: ${a--}<br>`);
+// document.write(` Now the value of a is: ${a}<br>`);
+
+
+
+// question 2
+// let a = 2;
+// let b = 1;
+// let res = (a , b );
+//  --a - --b + ++b + b--
+//   1  - 0   + 2   + 1
+// console.log( res)
+
+
+//  --a
+//  console.log(--a);
+//  -1
+
+// --a - --b;
+// 1   -  1
+// console.log(--a - --b);
+//            -1
+// console.log(-b);
+
+
+// --a - --b;
+// console.log(--a);
+
+// console.log(--a - --b);
+//              1  - -1 
+
+    //          console.log(--a);
+    //         console.log(--a - -b || b);
+    //                       -1
+           
+       
+                          
+    // console.log(--a - --b + ++b +b);
+    //                  -1     
+                          
+    //   question 3
+
+// write a program that takes input a name from user and greet the user
+  
+
+// let name = prompt("Please enter your name:");
+// alert(`Hello, ${name}! Welcome!`);
+     
+//   question 4
+// write a program to take input a  number from user and disply it multipicatio table on your browser.if user does not enter a new number, multipication table of 5should be disply by default    
+
+// let num = prompt("Enter a number:") || 5;
+// let table = "";
+
+// for (let i = 1; i <= 10; i++) {
+//   table += `${num} x ${i} = ${num * i}\n`;
+// }
+
+// alert(table);
+
+
+
+
+    
+    
+    
+
+    
+    
+    
+    
